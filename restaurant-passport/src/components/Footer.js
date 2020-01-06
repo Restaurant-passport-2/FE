fe6dc273282d6457e94acb777b5b7179f0c73ebb
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-06 14:37:50 -0500

    initial set up and working on footer

--- restaurant-passport/src/components/Footer.js
+++ restaurant-passport/src/components/Footer.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {NavLink} from 'react-router-dom';
 
 export default function Footer() {
     return (
-        <footer>
-
-        </footer>
+        <div>
+            <nav>
+                <NavLink to='https://restaurant-passport-team.netlify.com/index.html'>About</NavLink>
+                <NavLink to='https://restaurant-passport-team.netlify.com/about.html'>About Us</NavLink>
+                <NavLink to=''>My Passport</NavLink>
+            </nav>
+            <p>Restaurant Passport copyright The Restaurant Passport Team</p>
+       </div>
     );
 }
