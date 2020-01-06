21898794d145fdccf08b8b62be95439dfb74d5fa
Heather Nuffer <zzzuzz@gmail.com>
2020-01-06 12:11:39 -0700

    Initial layout for Header

--- restaurant-passport/src/components/Header.js
+++ restaurant-passport/src/components/Header.js
@@ -1,9 +1,33 @@
 import React from "react";
+import logo from '../images/logo.png';
+import { Route, Router, Link } from 'react-router-dom';
 
-export default function Header() {
+import Login from "./Login";
+import SignUp from "./SignUp";
+import RestaurantList from './RestaurantList';
+
+export default function Header(props) {
     return (
         <header>
-
+            <div className='img-container'>
+                    <img src={logo} alt='logo' />
+                </div>
+            <h1>Restaurant Passport</h1>
+            <nav>
+                <p>Sign Up</p>
+                <p>Log In</p>
+                <p>Sign Out</p>
+                <p>My Passport</p>
+            </nav>    
         </header>
     );
 }
+
+/*
+<nav>
+                <Link to='/signup'>Sign Up</Link>
+                <Link to='/login'>Log In</Link>
+                <Link to='/login'>Sign Out</Link>
+                <Link to='/restaurants'>My Passport</Link>   
+                </nav>
+*/
