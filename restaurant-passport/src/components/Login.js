b3cecbba37a1efab1e3cbc5f4a0bf668a9c725c5
Heather Nuffer <zzzuzz@gmail.com>
2020-01-06 15:41:09 -0700

    Added labels to login form

--- restaurant-passport/src/components/Login.js
+++ restaurant-passport/src/components/Login.js
@@ -2,117 +2,60 @@ import React, { useEffect, useState} from "react";
 import axios from 'axios';
 
 export default function Login() {
-    // const [state, setState] = useState ({userName: "", password: ""})
-    // return  (
-    //     <form>
-   
+  const [user, setUser] = React.useState({
+    username: "",
+    password: "",  
+  });
 
-    //     </form>
-    // );
-    
-        const [teamMembers, setTeamMembers] = React.useState([{
-            
-          },
-          
-        ])
-      
-      
-        
-      
-        return (
-          <div className="App">
-            <h1>Login Info</h1>
-      
-            <NewTeamMemberForm 
-           
-              teamMembers={teamMembers} 
-              setTeamMembers={setTeamMembers}
-            
-            />
-      
-            <TeamMembers 
-              teamMembers={teamMembers} 
-           
-             />
-          </div>
-        );
-      }
-      
-      function NewTeamMemberForm(props) {
-        const [newTeamMember, setNewTeamMember] = React.useState({
-          username: "",
-          password: "",
-          
-        })
-      
-        const handleSubmit = (event) => {
-          event.preventDefault()
-          axios.post("https://restaurant-passport-2.herokuapp.com/api/auth/login", newTeamMember)
-          .then(response =>{
-              console.log(response)
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post("https://restaurant-passport-2.herokuapp.com/api/auth/login", user)
+    .then(response =>{
+        console.log(response)
 
-          })
-          .catch(err => {
-              console.log(err)
-          })
+    })
+    .catch(err => {
+        console.log(err)
+    });
+  };
 
-        
-        }
-      
-        const handleChange = (event) => {
-          setNewTeamMember({...newTeamMember, [event.target.name]: event.target.value })
-        }
-      
-        return (
-      
-          <form onSubmit={handleSubmit}>
+  const handleChange = (event) => {
+    setUser({...user, [event.target.name]: event.target.value })
+  };
+
+  return (
+    <div className="App">
+      <h1>Access Your Passport</h1>
+      <form onSubmit={handleSubmit}>
+          <legend>Log In</legend>
           <div className="inputContainer">
-            <input 
+            <label htmlFor="username">Username: </label>
+            <input
+              required 
               type="text"
               name="username"
-              placeholder="enter your Username here"
+              id="username"
+              placeholder="enter your username here"
               onChange={handleChange}
-              value={newTeamMember.username}
+              value={user.username}
             />
-            </div>
+          </div>
       
-            <div className="inputContainer">
-              <input 
+          <div className="inputContainer">
+            <label htmlFor="password">Password: </label>
+              <input
+                required 
                 type="password"
                 name="password"
+                id="password"
                 placeholder="enter password here"
                 onChange={handleChange}
-                value={newTeamMember.password}
+                value={user.password}
               />
-         
             </div>
-      
-            
-      
-          {props.isEditing ? <button role="submit">Edit team member</button> : <button role="submit">Login</button>}
+            <button role="submit">Login</button>
         </form>
-        )
-      }
-      
-      function TeamMember({member, handleEdit, handleDelete}) { return (
-        <div className="team-member">
-         
-        </div>
-      )}
-      
-      function TeamMembers(props) {
-        const renderTeamMember = function mapperFn(member) {
-          return <TeamMember 
-                    member={member}
-                    handleEdit={props.handleEdit} 
-                    handleDelete={props.handleDelete}
-                  />
-        }
-      
-        return (
-          <div className="team-members">
-            {props.teamMembers.map(renderTeamMember)}
-          </div>
-        )
-      }
 
+    </div>
+  );
+  }
