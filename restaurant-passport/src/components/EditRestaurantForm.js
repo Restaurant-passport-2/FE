1d0c296ebf6f4c5d605b0c7855c33e2db46020c3
Ariana Shackelford <ariana.shackelford@gmail.com>
2020-01-07 18:14:51 -0500

    fixed bugs

--- restaurant-passport/src/components/EditRestaurantForm.js
+++ restaurant-passport/src/components/EditRestaurantForm.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState} from "react";
 import axios from 'axios';
 
-export default function AddRestaurant() {
+export default function EditRestaurantForm() {
     const [restaurantName, setRestaurantName] = React.useState({
         restaurantname: "",
         city: "",
