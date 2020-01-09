a5e0056498add3a7cf1dfc66e316c790470029b0
Heather Nuffer <zzzuzz@gmail.com>
2020-01-08 20:14:05 -0700

    Added content and styles to profile

--- restaurant-passport/src/components/Profile.js
+++ restaurant-passport/src/components/Profile.js
@@ -1,18 +1,46 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import utensils from '../images/utensils_noBackground.png';
+import forkLeft from '../images/forkLeft.png';
+import forkRight from '../images/forkRight.png';
+import menu from '../images/menu.png';
+
 const Profile = props => {
     const numVisited = props.passport.filter(restaurant => (restaurant.stamped)).length;
     return (
-        <div>
-        <h1>Passport Holder</h1>
-        <h2>Name: {props.name}</h2>
-        <p>Username: {props.username}</p>
-        <p>Email: {props.email}</p>
-        <p>Location: {props.city} {props.zipcode}</p>
-        <p>Restaurants in Passport: {props.passport.length}</p>
-        <p>Restaurants Visited So Far: {numVisited}</p>
-        <p>Restaurants Not Visited Yet: {props.passport.length - numVisited}</p>
+        <div className='profile-page'>
+
+            <div className='profile-box'>
+
+                <div className='title-box'>
+                    
+                    <div className='img-container'>
+                        <img src={forkLeft} alt='fork with broccoli' />
+                    </div>
+                    <h1>Passport Holder</h1>
+                    <div className='img-container'>
+                        <img src={forkRight} alt='fork with apple' />
+                    </div>
+                </div>
+
+                <div className='inner-profile-box'>
+                    <div className='img-container'>
+                        <img src={menu} alt='menu' />
+                    </div>
+                    <div className='text-box'>
+                        <h2><span className='profile-label'>Name:</span> {props.name}</h2>
+                        <hr />
+                        <p><span className='profile-label'>Username:</span> {props.username}</p>
+                        <p><span className='profile-label'>Email:</span> {props.email}</p>
+                        <p><span className='profile-label'>Location:</span> {props.city} {props.zipcode}</p>
+                        <p><span className='profile-label'>Restaurants in Passport:</span> {props.passport.length}</p>
+                        <p><span className='profile-label'>Restaurants Visited So Far:</span> {numVisited}</p>
+                        <p><span className='profile-label'>Restaurants Not Visited Yet:</span> {props.passport.length - numVisited}</p>
+                        <p><span className='profile-label'>Current Score:</span> {(numVisited/props.passport.length) * 100}%</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     )
@@ -23,8 +51,9 @@ const mapStateToProps = state => {
         ...state,
     username: state.username,
     name: state.name,
-    city: state.email,
-    zipcode: state.zipcode,
+    city: state.city,
+    email: state.email,
+    zipcode: state.zip,
     passport: state.passport
     };
 };
