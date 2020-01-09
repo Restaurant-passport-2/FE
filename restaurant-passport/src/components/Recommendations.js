b9c5e527eb3dfb81068a83cd2420a7b35dd9574e
Heather Nuffer <zzzuzz@gmail.com>
2020-01-08 22:40:46 -0700

    Book image displays when recommendation is added to passport and button disappears

--- restaurant-passport/src/components/Recommendations.js
+++ restaurant-passport/src/components/Recommendations.js
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import { getRecommendations} from '../actions/actions';
 import { addRestaurant } from '../actions/actions';
 
+import penBook from '../images/penBook.png';
+
 const Recommendations = props => {
 
     const fetchRecommendations = () => {
@@ -26,6 +28,15 @@ const Recommendations = props => {
             "notes": "Added this recommendation to my passport"
         }
         props.addRestaurant(formattedRestaurant);
+        const box = document.getElementById(`${restaurant.id}-box`);
+        const imgNode = document.createElement('img');
+        imgNode.src=penBook;
+        imgNode.alt='book with pen';
+        imgNode.style.width = '3rem';
+        box.append(imgNode);
+        const addButton = document.getElementById(`${restaurant.id}-button`);
+        addButton.style.display = 'none';
+        
     }
 
     return (
@@ -34,7 +45,7 @@ const Recommendations = props => {
             {!props.recommendations && <button onClick={fetchRecommendations}>See Restaurants In My Area</button>}
             {props.recommendations && props.recommendations.map(restaurant => {
                 return (
-                    <div key={restaurant.id}>
+                    <div id={`${restaurant.id}-box`} key={restaurant.id}>
                         <h2>{restaurant.name}</h2>
                         <p>{restaurant.location.address1}</p>
                         <p>{restaurant.location.city}, {restaurant.location.state} {restaurant.location.zip_code}</p>
@@ -45,9 +56,9 @@ const Recommendations = props => {
                         <p>Type(s):</p>
                         {restaurant.categories.map(category => <p key={category.title}>{category.title}</p>)}
                         <div className='yelp-img-box'>
-                            <img src={restaurant.image_url} alt='yelp photo' />
+                            <img src={restaurant.image_url} alt='from yelp' />
                         </div>
-                        <button onClick={() => addToPassport(restaurant)}>Add to My Passport</button>
+                        <button id={`${restaurant.id}-button`} onClick={() => addToPassport(restaurant)}>Add to My Passport</button>
                         <hr />
                     </div>
                 )
