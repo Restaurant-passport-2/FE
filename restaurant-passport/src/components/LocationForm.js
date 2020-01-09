c0fb7098fc481a2fef11413390e1d0dd7bacedb0
Heather Nuffer <zzzuzz@gmail.com>
2020-01-09 15:23:32 -0700

    Began creating a LocationForm

---
+++ restaurant-passport/src/components/LocationForm.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { fetchRecommendations } from '../actions/actions';
+import { connect } from "react-redux";
+
+const LocationForm = props => {
+    return (
+        <form>
+            <legend>Search By Location</legend>
+        </form>
+    )
+}
+export default LocationForm;
+
